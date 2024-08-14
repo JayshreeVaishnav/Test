77b96397de06efe41ac051f5189e78d75ec6f637
Student Demo <jayvaishnav111.jv@gmail.com>
2024-08-14 12:31:41 +0530

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+// import Header from './components/layouts/Header'
+import Home from './components/pages/homepage/Index'
+
+export default function App() {
+  return (
+<>
+   <Home>
+    
+   </Home>
+    </>
+  )
+}
---
+++ src/components/pages/homepage/Index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+<>
+
+
+    <div class="container">
+        <div class="row">
+            <div className="col-lg-3 col-md-6 col-sm-2">
+                 <img src="\public\images\images (1).jpg" alt=""/>
+                 <h4>Lorem, ipsum dolor.</h4>
+                 <h5>Lorem ipsum dolor sit amet.</h5>
+                 <h5>13-18 Aug</h5>
+                 <h6>7,999 night</h6>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-2">
+                        <img src="\public\images\images (1).jpg" alt=""/>
+                        <h4>Lorem, ipsum dolor.</h4>
+                        <h5>Lorem ipsum dolor sit amet.</h5>
+                        <h5>13-18 Aug</h5>
+                        <h6>7,999 night</h6>
+                    </div>
+            <div className="col-lg-3 col-md-6 col-sm-2">
+                <img  src="\public\images\images (1).jpg" alt=""/>
+                <h4>Lorem, ipsum dolor.</h4>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <h5>13-18 Aug</h5>
+                <h6>7,999 night</h6>    
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-2">
+                <img  src="\public\images\images (1).jpg" alt=""/>
+                <h4>Lorem, ipsum dolor.</h4>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <h5>13-18 Aug</h5>
+                <h6>7,999 night</h6>
+            </div>
+        </div>
+    </div>
+
+
+    </>
+   
+  )
+} 
+
+
+
